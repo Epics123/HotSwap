abe6fafa6c464c3a2cfc972f5ee10738148bbd2e
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2019-11-02 01:49:41 -0400

    Adding Item scriptable object

---
+++ HotSwap/Assets/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newItem", menuName = "Item")]
+public class Item : ScriptableObject
+{
+	public new string name;
+	public string description;
+	public uint weight;
+
+	List<Item> items = new List<Item>();
+}
