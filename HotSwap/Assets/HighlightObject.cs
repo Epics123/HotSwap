0e7adc4ef6a86c63d3f9b767f9e9655fa02c5393
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-11-02 14:16:40 -0400

    Adding HighlightObject script which highlights an object when hovered over

---
+++ HotSwap/Assets/HighlightObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public Material boarder;
+    public Material nonBoarder;
+
+    public bool isHighlighted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Highlight();
+    }
+
+
+    void Highlight()
+    {
+        if(isHighlighted)
+            GetComponent<Renderer>().material = boarder;
+        else
+            GetComponent<Renderer>().material = nonBoarder;
+    }
+
+    //Test highlighting with mouse
+    private void OnMouseOver()
+    {
+        isHighlighted = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isHighlighted = false;
+    }
+}
