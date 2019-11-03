96768f98ffe9ec81515d2f0d33d370068605e584
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-11-02 23:54:23 -0400

    Adding finished clock with flashing effects

--- HotSwap/Assets/Clock.cs
+++ HotSwap/Assets/Clock.cs
@@ -9,13 +9,15 @@ public class Clock : MonoBehaviour
     public TMP_Text clock;
     int hours, minutes;
     float timer = 0.0f;
-    int startHour;
+    int startHour, endHour, halfway;
 
     // Start is called before the first frame update
     void Start()
     {
         startHour = DateTime.Now.Hour;
         hours = startHour;
+        endHour = startHour + 2;
+        halfway = endHour - 1;
     }
 
     // Update is called once per frame
@@ -26,23 +28,43 @@ public class Clock : MonoBehaviour
 
     void CountTime()
     {
-
-        timer += Time.deltaTime;
-        minutes = Convert.ToInt32(timer % 60);
-
-        if (minutes >= 60)
+        if(hours != endHour)
         {
-            timer = 0.0f;
-            minutes = 0;
-            hours++;
-        }
-            
+            timer += Time.deltaTime;
+            minutes = Convert.ToInt32(timer % 60);
 
-        clock.text = LeadingZero(hours) + ":" + LeadingZero(minutes);
+            if (minutes >= 60)
+            {
+                timer = 0.0f;
+                minutes = 0;
+                hours++;
+            }
+
+            if (hours == halfway && minutes > 30)
+            {
+                ChangeClockColor();
+                FlashText();
+            }
+
+            if (hours >= endHour)
+                clock.alpha = 1.0f;
+
+            clock.text = LeadingZero(hours) + ":" + LeadingZero(minutes);
+        }
     }
 
     string LeadingZero(int time)
     {
         return time.ToString().PadLeft(2, '0');
+    }
+
+    void ChangeClockColor()
+    {
+        clock.color = Color.red;
+    }
+
+    void FlashText()
+    {
+        clock.alpha = Mathf.PingPong(Time.time * 2, 1.0f);
     }
 }
