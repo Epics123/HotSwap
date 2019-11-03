3fa6599463b86218d9c31c0a6a1704fc02858544
Aidan Murphy <aidan.murphy@mymail.champlain.edu>
2019-11-03 12:54:29 -0500

    Adding respawn script

---
+++ HotSwap/Assets/respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public List<GameObject> props;
+    public List<Vector3> spawnLocs;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "grabbable")
+        {
+            respawnObjects();
+        }
+    }
+
+    void respawnObjects()
+    {
+        int random = Random.Range(0, props.Count);
+        int randomLoc = Random.Range(0, spawnLocs.Count);
+        Instantiate(props[random], spawnLocs[randomLoc], Quaternion.identity);
+    }
+}
