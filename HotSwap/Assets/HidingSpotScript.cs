808a16d03a22d0b5d423e262b44d8568a68e2352
Wesley-Elmer <wesley.elmer@mymail.champlain.edu>
2019-11-02 20:49:21 -0400

    Created Script

---
+++ HotSwap/Assets/HidingSpotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class HidingSpotScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
